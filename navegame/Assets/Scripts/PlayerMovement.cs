95e0afb2c620a60eca2c8b4b6ea1339d3aef103c
VerXxuxZ <125389780+JulianSerranoChacon@users.noreply.github.com>
2024-07-26 22:09:00 +0200

    subo

--- navegame/Assets/Scripts/PlayerMovement.cs
+++ navegame/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,8 @@ using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] UnityEvent _onDerrape = new UnityEvent();
-    public UnityEvent OnDerrape => _onDerrape;
+//    [SerializeField] UnityEvent _onDerrape = new UnityEvent();
+//    public UnityEvent OnDerrape => _onDerrape;
 
     private Rigidbody2D _myRigidBody;
 
@@ -18,41 +18,11 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 _direction;
 
-    private bool _borracho;
-
-    private bool _derrape;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "mancha")
-        {
-            _borracho = true;
-        }
-        if(collision.gameObject.tag == "cigarro")
-        {
-            GetComponent<TobaccoNerf>().nerf();
-            collision.transform.parent.GetComponent<CreateFume>().Fumar();
-            Destroy(collision.gameObject);
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "mancha")
-        {
-            _borracho = false;
-        }
-    }
-
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
-        _direction *= _borracho ? -1 : 1;
     }
 
-    public void SetDerrape(bool derrape)
-    {
-        _derrape = derrape;
-    }
     public void addMaxSpeed(float moreSpeed)
     {
         _maxSpeed += moreSpeed;
@@ -60,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
-        GameManager.Instance.registerPlayerTransform(transform);
+        //GameManager.Instance.registerPlayerTransform(transform);
     }
 
     // Start is called before the first frame update
@@ -68,7 +38,6 @@ public class PlayerMovement : MonoBehaviour
     {
         _myRigidBody = GetComponent<Rigidbody2D>();
         _direction = Vector2.zero;
-        _borracho = false;
     }
 
     // Update is called once per frame
@@ -76,21 +45,5 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_myRigidBody.velocity.magnitude < _maxSpeed)
             _myRigidBody.AddForce(_direction * _force, ForceMode2D.Force);
-
-        SetDerrape(_derrape);
-
-        if (_derrape && _myRigidBody.velocity.magnitude < _maxSpeed)
-        {
-            _onDerrape.Invoke();
-            if(_myRigidBody.velocity.magnitude > 0 && _myRigidBody.velocity.x > 0)
-            {
-                _myRigidBody.AddForce(new Vector2(0, 1)*Time.deltaTime, ForceMode2D.Force);
-            }
-            else if(_myRigidBody.velocity.magnitude > 0 && _myRigidBody.velocity.x < 0)
-            {
-                _myRigidBody.AddForce(new Vector2(0, -1)*Time.deltaTime, ForceMode2D.Force);
-            }
-        }
-
     }
 }
