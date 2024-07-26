c4dd5dd2517ce6afd5849cda5e21490a56db2b59
samuel <samuga05@ucm.es>
2024-07-26 23:12:42 +0200

    input v1

--- navegame/Assets/Scripts/Input/InputTest.cs
+++ navegame/Assets/Scripts/Input/InputTest.cs
@@ -8,39 +8,110 @@ public class InputTest : MonoBehaviour
 
 
     public PlayerInput playerInput;
-    public string actionMapName;
 
+    private int currentActionMap;
 
+    public string actionMapBaseName;
+
+    private bool switchedThisFrame = false;
+
+    private bool leftPressed = false;
+    private bool rightPressed = false;
+    private bool upPressed = false;
+    private bool downPressed = false;
+    private bool shootPressed = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentActionMap = 1;
+
+        string currentActionMapName = actionMapBaseName + currentActionMap;
+
+        SwitchActionMap(currentActionMapName);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        switchedThisFrame = false;
     }
 
     public void moveLeft(InputAction.CallbackContext context)
     {
+        if(context.canceled) leftPressed = false;
+
         if(!context.started) return;
+
+        leftPressed = true;
 
         print("leftPressed");
 
-        //print(playerInput.currentActionMap.name);
-            
-        if(playerInput.currentActionMap.name != actionMapName)  
-        {
-            playerInput.SwitchCurrentActionMap(actionMapName);
-            //print("changedMap");
-            //print(playerInput.currentActionMap.name);
-            //playerInput.currentActionMap.Enable();  
-        }
+        currentActionMap = 2;
+        string currentActionMapName = actionMapBaseName + currentActionMap;
+
+        SwitchActionMap(currentActionMapName);
+    }
+    public void moveRight(InputAction.CallbackContext context)
+    {
+        if (context.canceled) rightPressed = false;
+
+        if (!context.started) return;
+        rightPressed = true;
+
+        print("rightPressed");
+    }
+    public void moveUp(InputAction.CallbackContext context)
+    {
+        if (context.canceled) upPressed = false;
+
+        if (!context.started) return;
+
+        upPressed = true;
+
+        print("upPressed");
+    }
+    public void moveDown(InputAction.CallbackContext context)
+    {
+        if (context.canceled) downPressed = false;
+
+        if (!context.started) return;
+
+        downPressed = true; 
+
+        print("downPressed");
+    }
+    public void shoot(InputAction.CallbackContext context)
+    {
+        if (context.canceled) shootPressed = false;
+
+        if (!context.started) return;
+
+        shootPressed = true;    
+
+        print("shootPressed");
     }
 
 
+    private void SwitchActionMap(string newMapName)
+    {
+        if (playerInput.currentActionMap.name != newMapName && !switchedThisFrame)
+        {
+            switchedThisFrame = true;
+            playerInput.SwitchCurrentActionMap(newMapName);
+            print("Action map switched to : " + newMapName);
+        }
+    }
+
+    public Vector2 getDir()
+    {
+        var vec = new Vector2(0, 0);
+
+        if (leftPressed) vec -= new Vector2(-1, 0);
+        if (rightPressed) vec -= new Vector2(1, 0);
+        if (upPressed) vec -= new Vector2(0,1);
+        if (downPressed) vec -= new Vector2(0, -1);
+
+        return vec;
+    }
 
 }
