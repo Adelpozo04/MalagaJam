e537c7c96445d0d614941559330ddc46ca02f919
DaHo39 <pamarc05@ucm.es>
2024-07-26 23:50:15 +0200

    Clase base ship component

---
+++ navegame/Assets/Scripts/ShipParts/ShipComponent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipComponent : MonoBehaviour
+{
+    #region parameters
+    private int compLife_;
+    #endregion
+
+    #region references
+    private ShipManager myShipMng_;
+    #endregion
+
+    #region methods
+    public void GotHit()
+    {
+
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myShipMng_ = this.transform.parent.GetComponent<ShipManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
