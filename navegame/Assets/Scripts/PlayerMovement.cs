5e1b6f56653ad66f95e16e8e7a700d9ed587f9cc
VerXxuxZ <125389780+JulianSerranoChacon@users.noreply.github.com>
2024-07-26 23:30:28 +0200

    ghcg

--- navegame/Assets/Scripts/PlayerMovement.cs
+++ navegame/Assets/Scripts/PlayerMovement.cs
@@ -10,18 +10,25 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _myRigidBody;
 
     [SerializeField] private float _force;
+    [SerializeField] private float _reductionForce;
+
+    [SerializeField] private bool rightMotorHadDestroyed;
+    [SerializeField] private bool leftMotorHadDestroyed;
+
+    [SerializeField] private InputTest _myInputTest;
 
     private Vector2 _direction;
 
-    public void SetDirection(Vector2 direction)
+    public void SetDirection()
     {
-        _direction = direction;
+        _direction = _myInputTest.getDir().normalized;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         _myRigidBody = GetComponent<Rigidbody2D>();
+        //_myInputTest = GetComponent<InputTest>();
         _direction = Vector2.zero;
     }
 
@@ -29,6 +36,13 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         //if (_myRigidBody.velocity.magnitude < _maxSpeed)
-            _myRigidBody.velocity = _direction * _force ;
+        SetDirection();
+        Debug.Log(_direction);
+
+        //if (rightMotorHadDestroyed && (_direction.y == 1 && !leftMotorHadDestroyed) && _direction.x != 0)
+        //{
+        //}
+        _myRigidBody.velocity = _direction * _force;
+
     }
 }
