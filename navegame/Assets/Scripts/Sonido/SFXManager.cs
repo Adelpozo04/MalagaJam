0aa90fcc88da9f99a8bb1d1d60b02c65a622f165
VerXxuxZ <125389780+JulianSerranoChacon@users.noreply.github.com>
2024-07-27 17:20:56 +0200

    subo los metodos del sonido

--- navegame/Assets/Scripts/Sonido/SFXManager.cs
+++ navegame/Assets/Scripts/Sonido/SFXManager.cs
@@ -6,8 +6,12 @@ public class SFXManager : MonoBehaviour
 {
     public static SFXManager instance;
 
-    [SerializeField] private AudioSource soundFXObject;
+    [SerializeField] private AudioSource soundFXObject; 
+    [SerializeField] private AudioSource soundFXObjectCont;
+    [SerializeField] private GameObject motorContObject;
 
+
+    [SerializeField] private AudioClip[] motorAudio = new AudioClip [3];
 
     private void Awake()
     {
@@ -42,5 +46,57 @@ public class SFXManager : MonoBehaviour
 
     }
 
+    public void startMotorsClip(Transform spawnTransform, float volume)
+    {
+        //Al principio
+        //spawn the game object
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        //assign audio clip 
+        audioSource.clip = motorAudio[0];
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play clip
+        audioSource.Play();
+
+        //duración del clip
+        float clipLenght = audioSource.clip.length;
+
+        Invoke("contAudio", clipLenght);
+
+        //destruye tras sonar
+        Destroy(audioSource.gameObject, clipLenght);
+    }
+
+    public void endMotorsClip(Transform spawnTransform, float volume)
+    {
+        if(motorContObject != null)
+            Destroy(motorContObject);
+
+        playSFXClip(motorAudio[2], motorContObject.transform, volume);
+    }
+
+    private void contAudio(Transform spawnTransform, float volume)
+    {
+
+        //spawn the game object
+        AudioSource audioSource = Instantiate(soundFXObjectCont, spawnTransform.position, Quaternion.identity);
+
+        //assign audio clip 
+        audioSource.clip = motorAudio[0];
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play clip
+        audioSource.Play();
+
+        //duración del clip
+        float clipLenght2 = audioSource.clip.length;
+
+        motorContObject = audioSource.gameObject;
+    }
 
 }
