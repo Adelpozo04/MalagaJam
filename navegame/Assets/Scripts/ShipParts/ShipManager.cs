b381745603ca2db5d1766782daa742108f8a102b
DaHo39 <pamarc05@ucm.es>
2024-07-27 11:16:55 +0200

    Cámara funciona (no testeado daño)
    también hay que mirar el shader de nuevo para ver que está pasando con los objetos

--- navegame/Assets/Scripts/ShipParts/ShipManager.cs
+++ navegame/Assets/Scripts/ShipParts/ShipManager.cs
@@ -5,10 +5,17 @@ using UnityEngine.Serialization;
 
 public class ShipManager : MonoBehaviour
 {
+    #region parameters
+    #endregion
+
+    #region references
     PlayerMovement _myPlayerMovement;
     [SerializeField] private FuelBarController fuelBar;
     [SerializeField] private int fuelDamage; //La cantidad de fuel que pierdes al ser golpeado sin componente
-    
+    //Yo diría que cada componente te pase el daño que vayan a sufrir, así si un componente es golpeado demasiado sufres más daño
+    #endregion
+
+    #region methods
     private void destroyRightMotor()
     {
         _myPlayerMovement.destroyRightMotor();
@@ -23,6 +30,7 @@ public class ShipManager : MonoBehaviour
     {
         fuelBar.SubstractFuel(fuelDamage);
     }
+    #endregion
 
     // Start is called before the first frame update
     void Start()
