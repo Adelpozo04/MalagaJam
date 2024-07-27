0da2268977d5c8f9497a731025d646a5f705ebf2
VerXxuxZ <125389780+JulianSerranoChacon@users.noreply.github.com>
2024-07-27 23:48:34 +0200

    subo se le va una oreja

--- navegame/Assets/Scripts/ShipParts/Speakers.cs
+++ navegame/Assets/Scripts/ShipParts/Speakers.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Speakers : ShipComponent
 {
     [SerializeField] private bool isRight;
+    [SerializeField] private AudioClip destroyClip;
 
     override public void GotHit()
     {
+        SFXManager.instance.playSFXClip(destroyClip, transform, 1f);
         SFXManager.instance.destroySpeaker(isRight);
     }
 }
