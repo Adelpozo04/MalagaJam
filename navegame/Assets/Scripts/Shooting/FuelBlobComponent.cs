26fff7c4a2fad9f5dc3dd683d6f14735af9fdabe
samuel <samuga05@ucm.es>
2024-07-28 01:24:59 +0200

    ajustes

--- navegame/Assets/Scripts/Shooting/FuelBlobComponent.cs
+++ navegame/Assets/Scripts/Shooting/FuelBlobComponent.cs
@@ -16,6 +16,9 @@ public class FuelBlobComponent : MonoBehaviour
     #region methods
     #endregion
 
+
+    [SerializeField] private float fuelAmount = 50;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,7 +44,7 @@ public class FuelBlobComponent : MonoBehaviour
 
             if(tumadre != null)
             {
-                tumadre.sufferDamage(-50);
+                tumadre.addFuel(fuelAmount);
                 Destroy(gameObject);
             }
         }
