f84b38c6c99d12da2cec41b0cc995964000da6ea
Diego M <you@example.com>
2024-07-28 01:18:34 +0200

    nuevo tutorial

--- navegame/Assets/Scripts/TutorialController.cs
+++ navegame/Assets/Scripts/TutorialController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class TutorialController : MonoBehaviour
 {
@@ -13,11 +9,11 @@ public class TutorialController : MonoBehaviour
     //2. La barra de gasolina explicando que si te quedas sin, pierdes
     //3. Los controles explicando que van a cambiar según te quedas sin gasolina
 
+    [SerializeField] private TMP_Text botonText;
     [SerializeField] private GameObject fuelBar;
     [SerializeField] private GameObject controls;
     [SerializeField] private TMP_Text title;
     [SerializeField] private TMP_Text desc;
-    [SerializeField] private Button button;
     
     private int _stage;
     private void Start()
@@ -43,7 +39,7 @@ public class TutorialController : MonoBehaviour
                 title.text = "¡Los sistemas de control están fallando!";
                 desc.text = "A medida que se consuma tu gasolina los controles cambiarán. Se te avisará con antelación. ¡Buena suerte!";
                 controls.SetActive(true);
-                button.text = "VOLVER";
+                botonText.text = "VOLVER";
                 break;
             case 3:
                 SceneManager.LoadScene(0);
